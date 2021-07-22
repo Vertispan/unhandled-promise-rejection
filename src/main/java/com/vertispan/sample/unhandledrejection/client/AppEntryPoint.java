e7e3c8e5441abbfd7e7409a2066912b21b1e3712
Colin Alworth <colin@colinalworth.com>
2021-07-22 09:14:22 -0500

    Add more examples

--- src/main/java/com/vertispan/sample/unhandledrejection/client/AppEntryPoint.java
+++ src/main/java/com/vertispan/sample/unhandledrejection/client/AppEntryPoint.java
@@ -5,20 +5,21 @@ import elemental2.dom.EventListener;
 import elemental2.dom.HTMLButtonElement;
 import elemental2.promise.Promise;
 import jsinterop.annotations.JsMethod;
+import jsinterop.base.Js;
 
 import static elemental2.dom.DomGlobal.*;
 
 public class AppEntryPoint implements EntryPoint {
 
-    // Helper to add a listener to gwt's own iframe, without a check if it is different from the host page
+    // Helper to add a listener to gwt's own iframe window, without a check if it is different from the host page
     @JsMethod(namespace = "<window>")
     private static native void addEventListener(String type, EventListener listener);
 
     @Override
     public void onModuleLoad() {
-        HTMLButtonElement rejectBtn = (HTMLButtonElement) document.createElement("button");
-        rejectBtn.textContent = "click to reject (from gwt)";
-        rejectBtn.onclick = e -> {
+        HTMLButtonElement rejectPromiseBtn = (HTMLButtonElement) document.createElement("button");
+        rejectPromiseBtn.textContent = "click to reject (from gwt)";
+        rejectPromiseBtn.onclick = e -> {
             console.log("Rejecting a promise in an unhandled way from gwt");
             new Promise<>((resolve, reject) -> {
                 setTimeout(reject::onInvoke, 100);
@@ -29,9 +30,9 @@ public class AppEntryPoint implements EntryPoint {
             return true;
         };
 
-        HTMLButtonElement throwBtn = (HTMLButtonElement) document.createElement("button");
-        throwBtn.textContent = "click to throw (from gwt)";
-        throwBtn.onclick = e -> {
+        HTMLButtonElement throwPromiseBtn = (HTMLButtonElement) document.createElement("button");
+        throwPromiseBtn.textContent = "click to throw (from gwt)";
+        throwPromiseBtn.onclick = e -> {
             console.log("Throwing in a promise in an unhandled way from gwt");
             new Promise<>((resolve, reject) -> {
                 setTimeout(resolve::onInvoke, 100);
@@ -40,7 +41,19 @@ public class AppEntryPoint implements EntryPoint {
             });
             return true;
         };
-        document.body.append(rejectBtn, throwBtn);
+        document.getElementById("promises").append(rejectPromiseBtn, throwPromiseBtn);
+
+        // ---
+
+        HTMLButtonElement throwBtn = (HTMLButtonElement) document.createElement("button");
+        throwBtn.onclick = e -> {
+            console.log("Throwing in an event handler in an unhandled way from gwt");
+
+            throw new IllegalStateException("throw exception");
+        };
+        throwBtn.textContent = "click to throw in an event handler (from gwt)";
+
+        document.getElementById("throws").append(throwBtn);
 
         // ---
 
@@ -50,10 +63,24 @@ public class AppEntryPoint implements EntryPoint {
         window.addEventListener("unhandledrejection", e -> {
             console.log("host page reject via gwt", e);
         });
-//        EventListener lambda = e -> {
-//            console.log("gwt noticed a unhandled reject", e);
-//        };
-//        addEventListener("unhandledrejection", lambda);
-//        window.addEventListener("unhandledrejection", lambda);
+        window.addEventListener("error", e -> {
+            Object error = Js.asPropertyMap(e).get("error");
+            if (error == null) {
+                error = "null";
+            }
+            console.log("gwt noticed a host page error " + fromObject(error).getClass());
+        });
+        addEventListener("error", e -> {
+            Object error = Js.asPropertyMap(e).get("error");
+            if (error == null) {
+                error = "null";
+            }
+            console.log("gwt noticed its own iframe error " + fromObject(error).getClass());
+        });
     }
+
+    private static native Throwable fromObject(Object obj) /*-{
+        //GWT2 impl using JSNI, see GWT.native.js for the j2cl impl
+        return @java.lang.Throwable::of(*)(obj);
+    }-*/;
 }
